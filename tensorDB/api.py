2f686229bbc2fcfc44e1cbca36e6c21d02f3d5bf
Jason <81380030+JKerbin@users.noreply.github.com>
2024-04-16 10:17:06 +0800

    Update api.py

--- tensorDB/api.py
+++ tensorDB/api.py
@@ -1,6 +1,9 @@
+import numpy as np
 from flask import Flask, request, jsonify
-from scripts.database import Mysql
 from gevent import pywsgi
+from scripts.database import Mysql
+from scripts import embedding
+from scripts import search
 
 app = Flask(__name__)
 
@@ -48,6 +51,26 @@ def add_pdf():
     mysql_instance = Mysql(user=user, pw=pw, db_name=db_name, tb_name=tb_name, host=db_host, port=db_port)
     mysql_instance.add_pdf(url)
     return jsonify({'message': 'PDF added successfully.'})
+
+
+@app.route('/query', methods=['POST'])
+def query():
+    data = request.get_json()
+    text = data['text']
+    query_tensor = [embedding.embedding(text)]
+    query_tensor = np.array(query_tensor).astype('float32')
+    user = data.get('user')
+    pw = data.get('pw')
+    db_name = data.get('db_name')
+    tb_name = data.get('tb_name')
+    db_host = request.args.get('host', default='localhost', type=str)
+    db_port = request.args.get('port', default=3306, type=int)
+    mysql_instance = Mysql(user=user, pw=pw, db_name=db_name, tb_name=tb_name, host=db_host, port=db_port)
+    tensors = mysql_instance.get_tensors_by_ind()
+    # faiss查询————精确搜索
+    ind = search.faiss_flat(64, tensors, query_tensor, 1)
+    res = mysql_instance.get_texts_by_ind(ind, ind)[0]
+    return jsonify({'index': int(ind), 'result': res})
 
 
 if __name__ == '__main__':
